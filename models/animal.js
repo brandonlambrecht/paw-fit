ddd9e10f63c946aecab30ee766dcaae7225ecb94
sepan mohammed <sepanmustafa@gmail.com>
2023-11-29 22:20:10 -0600

    Added animal-seeds.js and seeded some data in the file and .env file, also worked on models/index.js and models/animal.js

--- models/animal.js
+++ models/animal.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes, STRING } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
 
 class Animal extends Model {}
 
--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./User');
+const Animal = require('./animal')
+const Diet = require('./diet')
+const Health = require('./health')
+
 
 // Write Table relationships here
 
-module.exports = { User };
+module.exports = { 
+    User,
+    Animal,
+    Diet,
+    Health, 
+};
