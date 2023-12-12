d67a9d18bf587cb74845ad936e38f51292ac3b97
sepan mohammed <sepanmustafa@gmail.com>
2023-12-11 18:21:36 -0600

    debugged an issue witht the calendar route. the calendar can now be displated in a different page

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,12 +1,22 @@
-import { Calendar } from '@fullcalendar/core'
-import timeGridPlugin from '@fullcalendar/timegrid'
+// import { Calendar } from '@fullcalendar/core'
+// import timeGridPlugin from '@fullcalendar/timegrid'
 
-const calendar = new Calendar(calendarEl, {
-plugins: [timeGridPlugin],
-initialView: 'timeGridWeek',
-headerToolbar: {
-    left: 'prev,next',
-    center: 'title',
-    right: 'timeGridWeek,timeGridDay' // user can switch between the two
-}
-})
+// const calendar = new Calendar(calendarEl, {
+// plugins: [timeGridPlugin],
+// initialView: 'timeGridWeek',
+// headerToolbar: {
+//     left: 'prev,next',
+//     center: 'title',
+//     right: 'timeGridWeek,timeGridDay' // user can switch between the two
+// }
+// })
+// const calendarE = document.querySelector('#calendar')
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
